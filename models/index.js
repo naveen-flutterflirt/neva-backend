e5a2cdafaf8745d39e3665d67dd98d7f51e05521
Suman Kumar <suman.kumar@talkfever.com>
2026-08-23 21:13:29 +0530

    updated

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/database');
+const User = require('./user');
+const Category = require('./category');
+const Product = require('./product');
+const ProductImage = require('./productImage');
+const CustomPrintRequest = require('./customPrintRequest');
+const Order = require('./order');
+const OrderItem = require('./orderItem');
+
+// User <-> Order
+User.hasMany(Order, { foreignKey: 'userId', as: 'orders' });
+Order.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+// Order <-> OrderItem
+Order.hasMany(OrderItem, { foreignKey: 'orderId', as: 'items', onDelete: 'CASCADE' });
+OrderItem.belongsTo(Order, { foreignKey: 'orderId', as: 'order' });
+
+module.exports = {
+  sequelize,
+  User,
+  Category,
+  Product,
+  ProductImage,
+  CustomPrintRequest,
+  Order,
+  OrderItem,
+};
