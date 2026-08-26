7c9abeb0915d87d5169135d0e9ec40ba89076e3f
Suman Kumar <139070977+suman987654432@users.noreply.github.com>
2026-08-26 15:03:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -78,3 +78,28 @@ const startServer = async () => {
 };
 
 startServer();
+
+
+DATABASE_URL=postgresql://neondb_owner:npg_92dFteILXQqH@ep-polished-leaf-axxvocjx-pooler.c-4.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require
+PORT=5050
+JWT_SECRET=neva_ecommerce_super_jwt_secret_key_2026
+JWT_EXPIRES_IN=7d
+ADMIN_EMAIL=admin@nivashop.in
+ADMIN_PASSWORD=Niva@Admin2026
+
+#AWS S3 for Photo
+AWS_REGION=us-east-1
+AWS_ACCESS_KEY_ID=AKIAVL2QW4BO5E4M736G
+AWS_SECRET_ACCESS_KEY=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
+AWS_S3_BUCKET=neva-ecommerce
+
+# Payment Integration 
+RAZORPAY_KEY_ID=rzp_test_TSnKuVgteVheGX
+RAZORPAY_KEY_SECRET=ON7wJdzn7mp1CDUEjF5jtbej
+
+# SMTP Email Configuration (For Sending Replies)
+SMTP_HOST=smtp.office365.com
+SMTP_PORT=587
+SMTP_USER=info@flutterflirt.com
+SMTP_PASS=xmtwhgvpzqtgztnc
+SMTP_FROM=<info@flutterflirt.com> 
